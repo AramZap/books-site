9241b5b7d057f53266bd443d8c80b7603dde21b3
aramzap <aramzap@terpmail.umd.edu>
2025-04-04 15:21:47 -0400

    vercel changes

--- flask_app/config.py
+++ flask_app/config.py
@@ -3,5 +3,3 @@ import os
 # Stores all configuration values
 SECRET_KEY = os.urandom(16)
 MONGODB_HOST = os.environ.get('MONGODB_HOST')
-
-'mongodb+srv://aramzap:showpony@cluster0.agpqikk.mongodb.net/cmsc388j?retryWrites=true&w=majority&appName=Cluster0'
